b9ab137b6987bb23b4790f8a7bb682921a3f2483
VinogradovMihail <misha-vinogradov-1997@mail.ru>
2022-08-11 14:40:21 +0300

    Task003 done

---
+++ Task003/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача N3 Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+int[] massive = new int[10];
+int index = 0;
+
+while(index < massive.Length)
+{
+    massive[index] = new Random().Next(-99, 100);
+    index++;
+}
+
+index = 0;
+while(index < massive.Length - 1)
+{
+    Console.Write(massive[index] + ", "); 
+    index++;
+}
+Console.WriteLine(massive[index]);
+
+index = 0;
+int max = massive[0];
+while(index < massive.Length)
+{
+    if(max < massive[index])
+    {
+        max = massive[index];
+        index++;
+    }
+    else
+    {
+        index++;
+    }
+}
+Console.WriteLine("Максимальным числом в массиве является " + max);
+
+index = 0;
+int min = massive[0];
+while(index < massive.Length)
+{
+    if(min > massive[index])
+    {
+        min = massive[index];
+        index++;
+    }
+    else
+    {
+        index++;
+    }
+}
+
+Console.WriteLine("Минимальным числом в массиве является " + min);
+
+int diff = max - min;
+
+Console.WriteLine("Разница между минимальным и максимальным числом в массиве: " + diff);
