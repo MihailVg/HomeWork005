c9c744671b8b0acc241d3a3c86b3ca96419f8b5b
VinogradovMihail <misha-vinogradov-1997@mail.ru>
2022-08-11 14:02:39 +0300

    Task001 done

---
+++ Task001/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача N1 Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] massive = new int[5];
+int index = 0;
+while(index < massive.Length)
+{
+    massive[index] = new Random().Next(100, 1000);
+    index++;
+}
+
+index = 0;
+
+while(index < massive.Length - 1)
+{
+    Console.Write(massive[index] + ", ");
+    index++;
+}
+Console.WriteLine(massive[index]);
+
+Console.Write("Количество четных в массиве: ");
+
+index = 0;
+int count = 0;
+
+while(index < massive.Length)
+{
+    if(massive[index] % 2 == 0)
+    {
+        count ++;
+        index++;
+    }
+    else
+    {
+        index++;
+    }
+}
+
+Console.Write(count);
+
